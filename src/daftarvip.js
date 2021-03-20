411cd5f4ce4402a4a7b39b30ee7bf6b0ce188c0c
reidogadobot <netoff197@gmail.com>
2021-03-20 08:15:42 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/559887583208 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO BOT:*
+_https://chat.whatsapp.com/GwrEhjkp9S34o7wQD62IGt`
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 SHAOLIN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/559887583208*
+║
+╚═〘 NABUTO BOT 〙
+`
+}
+exports.iklan = iklan
